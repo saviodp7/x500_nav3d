9bc84126504f48234c63de568ee942c8d874f9f0
Salvatore Del Peschio <saviodp7@gmail.com>
2026-01-22 23:18:02 +0000

    NAV-35 Add x500_package

---
+++ x500_description/launch/display.launch.py
@@ -0,0 +1,32 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import Command
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory("x500_description")
+    xacro_file = os.path.join(pkg_share, "urdf", "x500.urdf.xacro")
+    rviz_config_file = os.path.join(pkg_share, "rviz", "x500.rviz")
+
+    robot_description = Command(["xacro ", xacro_file])
+
+
+    return LaunchDescription(
+        [
+            Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                output="screen",
+                parameters=[{"robot_description": robot_description}],
+            ),
+            Node(
+                package="rviz2",
+                executable="rviz2",
+                output="screen",
+                arguments=["-d", rviz_config_file],
+            ),
+        ]
+    )
